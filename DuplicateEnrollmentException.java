5f54543b95a01d364a6ace045b376ff789aa4ef5
Nitu Bathyal <nbathyal0507@gmail.com>
2025-09-25 03:29:43 +0530

    initial commit

---
+++ DuplicateEnrollmentException.java
@@ -0,0 +1,13 @@
+package edu.ccrm.exceptions;
+
+public class DuplicateEnrollmentException extends Exception {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DuplicateEnrollmentException(String msg) { 
+    	super(msg);
+    	}
+}
+
---
+++ MaxCreditLimitExceededException.java
@@ -0,0 +1,11 @@
+package edu.ccrm.exceptions;
+
+public class MaxCreditLimitExceededException extends Exception {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public MaxCreditLimitExceededException(String msg) { super(msg); }
+}
+
